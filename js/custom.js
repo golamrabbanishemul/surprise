d391b33e1efadf751bf02283d4ca7394daf1e947
golamrabbanishemul <37244727+golamrabbanishemul@users.noreply.github.com>
2018-03-23 01:14:27 +0600

    design 4

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,23 @@
-// dropdown-toggle class not added for submenus by current WP Bootstrap Navwalker as of November 15, 2017.
-$('.dropdown-menu > .dropdown > a').addClass('dropdown-toggle');
+$( document ).ready( function () {
+    $( '.dropdown-menu a.dropdown-toggle' ).on( 'mouseenter', function ( e ) {
+        var $el = $( this );
+        var $parent = $( this ).offsetParent( ".dropdown-menu" );
+        if ( !$( this ).next().hasClass( 'show' ) ) {
+            $( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
+        }
+        var $subMenu = $( this ).next( ".dropdown-menu" );
+        $subMenu.toggleClass( 'show' );
 
-$('.dropdown-menu a.dropdown-toggle').on('mouseover', function(e) {
-    if (!$(this).next().hasClass('show')) {
-        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-    }
-    var $subMenu = $(this).next(".dropdown-menu");
-    $subMenu.toggleClass('show');
-    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-        $('.dropdown-menu > .dropdown .show').removeClass("show");
-    });
-    return false;
-});
+        $( this ).parent( "li" ).toggleClass( 'show' );
+
+        $( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
+            $( '.dropdown-menu .show' ).removeClass( "show" );
+        } );
+
+         if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
+            $el.next().css( { "top": $el[0].offsetTop, "right": $parent.outerWidth() - 4 } );
+        }
+
+        return false;
+    } );
+} );
